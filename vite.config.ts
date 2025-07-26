a75dae54222149632787b7beddfbbfb1bdb3cc4d
Amin Safavi <farshid.safavi@gmail.com>
2025-07-25 18:21:14 -0600

    fix git pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/package-scanner/', // for github pages deployment
   plugins: [react()],
   server: {
     proxy: {
